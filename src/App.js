a4fc047a53c67cd4d0faed5c2ad24b4b52219755
Nate Lee <nleepercussion@gmail.com>
2021-07-26 14:14:17 -0400

    Started adding additional components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import SearchForm from './components/SearchForm';
+import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <SearchForm />
+      <Router >
+       <SearchForm />
+      </Router>
     </div>
   );
 }
---
+++ src/components/UserList.jsx
@@ -0,0 +1,6 @@
+import UserProfile from "./UserProfile";
+import { useParams } from 'react-router-dom';
+
+export default (props) => {
+    const { id } = useParams();
+}
